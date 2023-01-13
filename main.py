ea8d5aaa0813c28c2459319c9c7d263c03e9a2bb
Cezary Czetyrba <99906101+Aliaser1001@users.noreply.github.com>
2023-01-13 17:42:07 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,98 +1,82 @@
-from urllib.parse import urljoin
-from urllib.error import URLError, HTTPError, ContentTooShortError
-from urllib import robotparser
-import urllib.request
-import re
-import Throttle
-from lxml.html import fromstring, tostring
-import cssselect
-
-url = "https://webscraper.io/test-sites/e-commerce/allinone"
-r_url = "https://webscraper.io/robots.txt"
-# Because the page was bigger than the example e-commerce shop I have manually written "robots.txt" url
-user_agent = "cc"
+import concurrent.futures
+from lxml.html import fromstring
+import Scraper #
 
 
-def get_robots_parser(robots_url):
-    # But if we start with normal site, then this function will read the "robots.txt" file
-    rp = robotparser.RobotFileParser()
-    rp.set_url(robots_url)
-    rp.read()
-    return rp
+Collin_url_1 = "https://www.collincad.org/propertysearch?prop_id="
+Collin_url_2 = "&situs_street_suffix=&isd%5B%5D=any&city%5B%5D=any&prop_type%5B%5D=R&prop_type%5B%5D=P&prop_type%5B%5D=MH&active%5B%5D=1&year=2022&sort=G"
 
 
-def download(url, user_agent="cc", num_retries=2, charset='utf-8'):
-    print("Downloading the:", url)
-    request = urllib.request.Request(url)
-    request.add_header('User_agent', user_agent)
-    # Trying to open and download the html file
-    try:
-        resp = urllib.request.urlopen(request)
-        cs = resp.headers.get_content_charset()
-        if not cs:
-            cs = charset
-        html = resp.read().decode(cs)
-    except (URLError, HTTPError, ContentTooShortError) as e:
-        print("Download error:", e.reason)
-        html = None
-        if num_retries > 0:
-            # Checking if the problem is with the client or something else and recursively downloading the html file
-            if hasattr(e, "code") and 500 <= e.code <= 600:
-                return download(url, num_retries - 1)
-
-    return html
+file = open("new_data_Collin.csv", 'r').readlines()
 
 
-def get_links(html):
-    # This function gets all the important links from crawled web page using regex
-    webpage_regex = re.compile("""<a[^>]+href=["'](/test-sites/e-commerce/allinone.*?)["']""", re.IGNORECASE)
-    return webpage_regex.findall(html)
+headers = {
 
 
-def write2csv(A):
-    file = open("data.csv", "a")
-    file.write(";"+str(A))
-    file.close()
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-GB,en;q=0.9",
+    "Host": "httpbin.org",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.2 Safari/605.1.15",
+    "X-Amzn-Trace-Id": "Root=1-63baa1f0-7fd6092b70f6532741e1d6bb"
 
 
-def link_crawler(start_url, robots_url, delay, user_agent="cc", max_depth=5):
-    # Creating the object of Throttle class to implement cooldown between each download
-    throttle = Throttle.Throttle(delay)
-    crawl_queue = [start_url]
-    seen = {}
-    """if not robots_url:
-        robots_url = "{}/robots.txt".format(start_url)"""
-    rp = get_robots_parser(robots_url)
-    while crawl_queue:
-        url = crawl_queue.pop()
-        if rp.can_fetch(user_agent, url):
-            depth = seen.get(url,0)
-            if depth == max_depth:
-                # In case of spider traps
-                print('Skipping %s due to depth' % url)
-                continue
-            throttle.wait(url)
-            html = download(url, user_agent=user_agent)
-            if not html:
-                # Checking if the current url provides something
-                continue
-            if "/product/" in url:
-                # Writing to .csv file description from product's site
-                write2csv(url)
-                tree = fromstring(html)
-                td = tree.cssselect('.caption')[0]
-                for index in td:
-                    info = index.text_content()
-                    write2csv(str(info))
-                write2csv("\n")
-            for link in get_links(html):
-                # Creates the next link to crawl
-                abs_link = urljoin(start_url, link)
-                if abs_link not in seen:
-                    seen[abs_link] = depth+1
-                    crawl_queue.append(abs_link)
-        else:
-            print("Blocked by robots.txt:", url)
+}
+### From csv file of ~500000 rows I got the IDs of Collin County to put into the main link to access pages needed to be scraped, then I've written it into an another csv file to make it faster to load during writting and debugging the scraper
 
 
-link_crawler(url, r_url, 1)
+"""file = open("City_Of_Dallas_Parcel_IDs.csv", "r").readlines()
+splitted_file = []
+data = {"Dallas": [], "Kaufman": [], "Denton": [], "Collin": [], "Rockwall": []}
+for i in file:
+    splitted_file.append(i.split(";"))
+for record in splitted_file[1:]:
+    if record[3] == "Dallas":
+        data["Dallas"].append(record[1])
+    elif record[3] == "Denton":
+        data["Denton"].append(record[1])
+    elif record[3] == "Collin":
+        data["Collin"].append(record[1])
+    elif record[3] == "Kaufman":
+        data["Kaufman"].append(record[1])
+    else:
+        data["Rockwall"].append(record[1])
+
+new_file_Collin = open("new_data_Collin.csv","a")
+for j in data["Collin"]:
+    new_file_Collin.writelines(j[4:]+";"+"\n")
+"""
+
+
+scraper = Scraper
+new_file_Collin = open("new_data_Collin.csv", "r").readlines()
+final_data_Collin = open("final_data_Collin1.csv", "a")
+
+
+
+
+
+def get_Collin(link):
+    body = scraper.download(link)
+
+    tree = fromstring(body.html)
+    td = tree.xpath('//div[@class="propertyinfo_container"]/div[@class ="propertyinfo"]/dl /dt')
+    headers= []
+    for index1 in td:
+        info1 = index1.text_content()
+        headers.append(info1)
+
+    td2 = tree.xpath('//div[@class="propertyinfo_container"]/div[@class ="propertyinfo"]/dl /dd')
+    data = []
+    for index2 in td2:
+        info2 = index2.text_content()
+        data.append(info2)
+
+crawl_queue = []
+
+for parcelID in new_file_Collin:
+    crawl_queue.append(Collin_url_1 + parcelID[:-2] + Collin_url_2)
+with concurrent.futures.ThreadPoolExecutor() as executor: # I used concurent futures module to make the scraper work a lot faster
+    executor.map(get_Collin, crawl_queue)
+
+
